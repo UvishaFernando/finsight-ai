f2c3f3a5a41587e9ef52e84b31456608f5d6591e
Uvisha Fernando <uvishafernando@gmail.com>
2026-05-12 12:49:20 +0530

    add  budget API endpoints

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import auth
+from app.api.v1.endpoints import auth,expenses
 
 api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(auth.router)
+api_router.include_router(expenses.router)
